5d5e8d4774ba049f0139e4ae751a72ecbce52836
nirmalraj <32131587+nirjo@users.noreply.github.com>
2020-08-09 23:58:17 +0530

    Fetching results from the query

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React ,{ useEffect } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { client } from './src/graphql/Client'
+import { Headlines } from './src/graphql/Queries'
+
 
 const Header = () => (
   <View style={styles.header}>
     <Text style={styles.headerText}>News App</Text>
   </View>
 )
-
+// Using the query Headlines the result should be fetched from the API whenever the component mounts as well as on the initial render of the app.
+// This can be done by using the React Hook useEffect.
 function App() {
+  useEffect(() => {
+    requestHeadlines()
+  }, [])
+
+  //Create a handler method requestHeadlines to invoke the query using Apollo Client. Invoking the query is simply done adding a promise.
+
+const requestHeadlines = () => {
+  client 
+    .query({
+      query: Headlines
+    })
+    .then(response => {
+      console.log('RESPONSE ===>', response)
+    })
+.catch(error => {
+  console.log('ERROR ==>', error)
+})
+}
+
   return (
     <View style={styles.container}>
       <Header />
