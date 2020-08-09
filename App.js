742c3672ba6f35bf178b3cde13d7177b49ef8f92
nirmalraj <32131587+nirjo@users.noreply.github.com>
2020-08-09 22:34:36 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const Header = () => (
+  <View style={styles.header}>
+    <Text style={styles.headerText}>News App</Text>
+  </View>
+)
+
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.contentContainer}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#C977CC'
   },
-});
+  header: {
+    marginTop: 50,
+    alignItems: 'center',
+    borderBottomWidth: StyleSheet.hairlineWidth
+  },
+  headerText: {
+    marginBottom: 5,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  }
+})
+
+export default App
